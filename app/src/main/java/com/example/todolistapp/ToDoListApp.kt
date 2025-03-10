427816765be078b3c16e8441543df6d0a5a85354
MarcinRod <rodziewicz.marcin@gmail.com>
2025-03-10 10:22:42 +0100

    Initial commit

---
+++ app/src/main/java/com/example/todolistapp/ToDoListApp.kt
@@ -0,0 +1,18 @@
+@file:Suppress("FunctionName")
+package com.example.todolistapp
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+
+@Composable
+fun ToDoListApp (modifier: Modifier = Modifier) {
+
+}
+
+@Preview
+@Composable
+fun ToDoListAppPreview() {
+    ToDoListApp()
+}
---
+++ app/src/main/java/com/example/todolistapp/ui/AddEditScreen.kt
@@ -0,0 +1,20 @@
+@file:Suppress("FunctionName")
+package com.example.todolistapp.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.todolistapp.ui.theme.ToDoListAppTheme
+
+@Composable
+fun AddEditScreen(modifier: Modifier = Modifier) {
+
+}
+
+@Preview
+@Composable
+fun AddEditScreenPreview() {
+    ToDoListAppTheme {
+        AddEditScreen()
+    }
+}
