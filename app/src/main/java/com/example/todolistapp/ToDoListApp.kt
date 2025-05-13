ece8d57cf4743562b93ff13761d610e967c2a350
Bartosz Berezowski <bberezowski7@gmail.com>
2025-05-13 13:39:28 +0200

    add top Bar fun

--- app/src/main/java/com/example/todolistapp/ToDoListApp.kt
+++ app/src/main/java/com/example/todolistapp/ToDoListApp.kt
@@ -1,38 +1,68 @@
 @file:Suppress("FunctionName")
 package com.example.todolistapp
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.example.todolistapp.data.DataSource
 import com.example.todolistapp.ui.AddEditScreen
 import com.example.todolistapp.ui.ToDoListScreen
 
-enum class ToDoAppDestinations() {
-    List,
-    Add,
-    Edit
+enum class ToDoAppDestinations(@StringRes val title: Int) {
+    List(title = R.string.list_screen_title),
+    Add(title = R.string.add_screen_title),
+    Edit(title = R.string.edit_screen_title)
 }
 
 @Composable
-fun ToDoListApp (modifier: Modifier = Modifier) {
-    val navController: NavHostController = rememberNavController()
+fun ToDoListApp(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController() // Kontroler nawigacji Jetpack Compose
+) {
+    // Pobiera bieżący wpis stosu wstecznego jako stan, aby interfejs użytkownika mógł reagować na zmiany nawigacji
+    val backStackEntry by navController.currentBackStackEntryAsState()
+
+    // Wyodrębnia nazwę ekranu z trasy. Usuwa wszelkie argumenty po "/", np. "Edit/{taskId}" staje się "Edit"
+    val screenName = backStackEntry?.destination?.route?.substringBefore("/")
+
+    // Określa bieżący ekran na podstawie nazwy trasy. Domyślnie ustawia na List, jeśli nazwa ekranu jest null (np. przy pierwszym uruchomieniu)
+    val currentScreen = ToDoAppDestinations.valueOf(
+        screenName ?: ToDoAppDestinations.List.name
+    )
+
     Scaffold(
-        topBar = {},
+        topBar = {
+            ToDoListTopBar(
+                currentScreen = currentScreen,
+                canNavigateBack = navController.previousBackStackEntry != null,
+                navigateUp = { navController.navigateUp() },
+                modifier = modifier.padding(start = 8.dp, end = 8.dp)
+            )
+        },
         floatingActionButton = {
             FloatingActionButton(onClick = {
                 navController.navigate(ToDoAppDestinations.Add.name)
@@ -51,7 +81,7 @@ fun ToDoListApp (modifier: Modifier = Modifier) {
         ) {
             composable(route = ToDoAppDestinations.List.name) {
                 ToDoListScreen(
-                    onEdit = { taskToEdit-> navController.navigate(ToDoAppDestinations.Edit.name + "/${taskToEdit.taskId}") },
+                    onEdit = { taskToEdit -> navController.navigate(ToDoAppDestinations.Edit.name + "/${taskToEdit.taskId}") },
                 )
             }
             composable(route = ToDoAppDestinations.Add.name) {
@@ -60,7 +90,7 @@ fun ToDoListApp (modifier: Modifier = Modifier) {
                         DataSource.addTask(it)
                         navController.navigateUp()
                     },
-                    onCancel = {navController.navigateUp()}
+                    onCancel = { navController.navigateUp() }
                 )
             }
             composable(route = ToDoAppDestinations.Edit.name) {
@@ -84,11 +114,32 @@ fun ToDoListApp (modifier: Modifier = Modifier) {
                         DataSource.updateTask(it)
                         navController.navigateUp()
                     },
-                    onCancel = {navController.navigateUp()}
+                    onCancel = { navController.navigateUp() }
                 )
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ToDoListTopBar(
+    currentScreen: ToDoAppDestinations,
+    canNavigateBack: Boolean,
+    navigateUp: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TopAppBar(
+        title = { Text(text = stringResource(id = currentScreen.title)) },
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back")
+                }
+            }
+        },
+        modifier = modifier.padding(start = 8.dp, end = 8.dp)
+    )
 }
 
 @Preview
